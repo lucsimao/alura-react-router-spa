989499223c4a5e0e313b85de891be7f291fc6df2
lucsimao <lucasimao64@gmail.com>
2022-02-16 19:09:06 -0300

    feat: create 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './assets/css/base/base.css';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import Home from './paginas/Home';
+import Pagina404 from './paginas/Pagina404';
 import React from 'react';
 import Sobre from './paginas/Sobre';
 
@@ -15,6 +16,9 @@ function App() {
         </Route>
         <Route path="/sobre">
           <Sobre />
+        </Route>
+        <Route>
+          <Pagina404 />
         </Route>
       </Switch>
     </Router>
---
+++ src/paginas/Pagina404.jsx
@@ -0,0 +1,15 @@
+import '../assets/css/404.css';
+
+import React from 'react';
+import imagem from '../assets/img/doguito404.svg';
+
+const Pagina404 = () => {
+  return (
+    <main className="container flex flex--centro flex--coluna">
+      <img className="doguito-imagem" src={imagem} alt="ilustração doguito" />
+      <p className="naoencontrado-texto">Ops, essa página não existe</p>
+    </main>
+  );
+};
+
+export default Pagina404;
