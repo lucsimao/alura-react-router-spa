84908c66a9c9e4320f94ca441a51491b44c2b3e9
lucsimao <lucasimao64@gmail.com>
2022-02-16 19:20:57 -0300

    feat: create cabecalho

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './assets/css/base/base.css';
 
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
+import Cabecalho from './components/Cabecalho';
 import Home from './paginas/Home';
 import Pagina404 from './paginas/Pagina404';
 import React from 'react';
@@ -10,6 +11,7 @@ import Sobre from './paginas/Sobre';
 function App() {
   return (
     <Router>
+      <Cabecalho />
       <Switch>
         <Route exact path="/">
           <Home />
