3ee5bb4e1244a28b10c46b02e584c384e9b26edb
lucsimao <lucasimao64@gmail.com>
2022-02-19 10:53:52 -0300

    feat: create Post page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Cabecalho from './components/Cabecalho';
 import Home from './paginas/Home';
 import Pagina404 from './paginas/Pagina404';
+import Post from './paginas/Post';
 import React from 'react';
 import Sobre from './paginas/Sobre';
 
@@ -18,6 +19,9 @@ function App() {
         </Route>
         <Route path="/sobre">
           <Sobre />
+        </Route>
+        <Route path="/posts/:id">
+          <Post />
         </Route>
         <Route>
           <Pagina404 />
