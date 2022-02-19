48c181ae96f2f9881a946e20147814d7d0dc7fd8
lucsimao <lucasimao64@gmail.com>
2022-02-19 10:42:59 -0300

    feat: add path param to ListaPost component link

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,5 @@ export const api = axios.create({ baseURL: 'http://localhost:5000/' });
 
 export const busca = async (url, setDado) => {
   const resposta = await api.get(url);
-  console.log(resposta.data);
   setDado(resposta.data);
 };
--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -13,7 +13,10 @@ const ListaPost = ({ url }) => {
   return (
     <section className="posts container">
       {posts.map((post) => (
-        <Link className={`cartao-post cartao-post--${post.categoria}`}>
+        <Link
+          to={`/posts/${post.id}`}
+          className={`cartao-post cartao-post--${post.categoria}`}
+        >
           <article key={post.id}>
             <h3 className="cartao-post__titulo">{post.title}</h3>
             <p className="cartao-post__meta">{post.metadescription}</p>
